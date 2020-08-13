dfff684bcccb8400e8dde5aae366611ea0eca0cd
Chaim Bryski <luzybry94@gmail.com>
2020-08-13 22:12:14 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    self.split(/[.?!]/).delete_if {|s| s.count < 2 }.count
+    self.split(/[^.?!][.?!]/).delete_if {|s| s.count < 2 }.count
     # binding.pry
   end
   # binding.pry
